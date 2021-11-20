01000e08435e4d2bcf09432b83457688b4745097
sarswatimata <94732790+sarswatimata@users.noreply.github.com>
2021-11-20 21:58:11 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,8 @@
+window['handleLoadSettings'] = function (settings) {
+	alert("Settings have been loaded!");
+};
 
+var script = document.createElement("script");
+script.src = "http://my-app.com/widget/settings?callback=handleLoadSettings";
+
+document.head.appendChild(script);
