6361a00ee3c05b4c79129ba1f6ddbe921fb993f8
sarswatimata <94732790+sarswatimata@users.noreply.github.com>
2021-11-21 19:19:19 +0530

    script2.js

--- script1.js
+++ script1.js
@@ -1,8 +1,16 @@
-window['handleLoadSettings'] = function (settings) {
-	alert("Settings have been loaded!");
+
+var num = Math.round(100000*Math.random())
+var loadsetting = "LoadSetting_" + num;
+
+
+
+window[loadsetting] = function (settings) {
+	alert("the title is " +settings.Title);
 };
 
 var script = document.createElement("script");
-script.src = "https://8b66-2409-4063-6e1e-69b-c007-9f6d-c89a-18b6.ngrok.io/setting?callback=handleLoadSettings";
+script.id = "script_" + loadsetting;
+script.src = "https://8b66-2409-4063-6e1e-69b-c007-9f6d-c89a-18b6.ngrok.io/setting?callback=" + loadsetting;
 
-document.head.appendChild(script);
+document.body.appendChild(script);
+document.getElementById(script.id).remove();
