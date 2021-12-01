f22ad57bb70e79b487ce62af656d9c233813ed42
sarswatimata <94732790+sarswatimata@users.noreply.github.com>
2021-12-01 21:13:51 +0530

    Update script9.js

--- script9.js
+++ script9.js
@@ -10,7 +10,7 @@ window['loadsetting'] = function (settings) {
 var script = document.createElement("script");
 
 script.id = "script_" + version;
-script.src = "https://e22f-2405-204-a52c-633f-e942-4708-430a-c7e7.ngrok.io/setting?callback=loadsetting&version="+version;
+script.src = "https://b9c1-2409-4063-4d13-5038-8df5-a952-199e-e8c1.ngrok.io/setting?callback=loadsetting&version="+version;
 
 
 document.body.appendChild(script);
