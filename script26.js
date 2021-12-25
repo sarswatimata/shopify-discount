83ae879036f376240463de2cd8b0539dbd1356c1
sarswatimata <94732790+sarswatimata@users.noreply.github.com>
2021-12-26 01:43:58 +0530

    Update script26.js

--- script26.js
+++ script26.js
@@ -14,17 +14,46 @@ window['loadsetting'] = function (settings) {
 	 if(array_url.includes("products")){
 	 var index =array_url.indexOf("products")
 	 var fbProduct =array_url[index+1]
+	  !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+           n.queue=[];t=b.createElement(e);t.async=!0;
+           t.src=v;s=b.getElementsByTagName(e)[0];
+           s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+           fbq('init', settings.Title );
+           fbq('trackCustom', 'Products', {name:fbProduct});		 
 	 console.log(fbProduct)
 	 	 
 	 }
 	else if(array_url.includes("collections")){
           var index_collection = array_url.indexOf("collections")
 	  var fbCollections = array_url[index_collection+1]
+	  !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+           n.queue=[];t=b.createElement(e);t.async=!0;
+           t.src=v;s=b.getElementsByTagName(e)[0];
+           s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+           fbq('init', settings.Title );
+	  fbq('trackCustom', 'Collection', {name: fbCollections});
+	  
 	  console.log(fbCollections)
+	}
+	else if(array_url.includes("cart")){
+	 console.log("cart")
+	}
+	else if(array_url.includes("checkouts")){
+	console.log("checkouts")
 	}
 	else{
 	var host = window.location.host
-		
+	var visit = 1
+	sessionStorage.setItem('test', visit)
+        if(visit != 1){		
 	!function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -36,6 +65,11 @@ window['loadsetting'] = function (settings) {
   fbq('init', settings.Title );
   fbq('track', 'PageView');
   		
+	}
+else{
+  console.log("visited")
+}	
+	
 	}}
 var script = document.createElement("script");
 
