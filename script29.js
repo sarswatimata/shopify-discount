f8c24955667b8583ac9678b87533e3e4edfd140b
sarswatimata <94732790+sarswatimata@users.noreply.github.com>
2021-12-25 13:53:46 +0530

    Create script29.js

---
+++ script29.js
@@ -0,0 +1,10 @@
+var num = Math.round(100000*Math.random())
+var version = num;
+console.log(version)
+var meta = document.createElement("meta");
+
+meta.id = "meta_" + version;
+meta.name = "facebook-domain-verification" 
+meta.content = "bqcpdwzx65bi7kje1vozrbs32skhoq"
+
+document.body.appendChild(meta);
